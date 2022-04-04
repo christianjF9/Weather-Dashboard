a1209aec5335f792c6efcf80abb11410f7c588b3
Christian Foote <christianfoote999@gmail.com>
2022-04-03 23:10:39 -0400

    added uv index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
 
-weather = JSON.parse(localStorage.getItem("test"));
+//weather = JSON.parse(localStorage.getItem("test"));
 var searchHistory = [];
+var currentDay = $("#current-day");
+var form = $("form");
+var historyEl = $("#history");
 
 
 var GenerateWeatherInfo = function(response){
     var api = "https://api.openweathermap.org/data/2.5/onecall?lat="+response[0].lat+"&lon="+response[0].lon+"&appid=ec93ec889e22ec6a9e1c57a53cc4c613"
     var weather = fetch (api).then(function(response){
    response.json().then(function(data){ //console.log("inside",data)
+    GenerateUv(data);
         $(".day").each(function(index, el){
-        console.log($(el));
+
         FillWeatherData($(el),data.daily[index]);    
         });
  //   localStorage.setItem("test",JSON.stringify(data));
@@ -17,12 +21,8 @@ var GenerateWeatherInfo = function(response){
 } 
 var  FillWeatherData = function(container, data){
     dataField =  container.children(".data");
-    //console.log(weather);
-    console.log(data.temp);
-    console.log(dataField);
     dataField[0].textContent = new Date(data.dt*1000).toLocaleDateString("en-US");
     var cloudInfo = data.weather[0];
-    console.log(data.weather[0])
     var cloudEl = container.children(".cloud");
     var cloudimg = "http://openweathermap.org/img/wn/"
     +cloudInfo.icon+"@2x.png";
@@ -37,35 +37,45 @@ var  FillWeatherData = function(container, data){
 }
 
 
-var currentDay = $("#current-day");
-//
-//console.log(moment("/Date(1318781876)/").format());
 
-var today = 
-console.log(today)
 
 //FillWeatherData(currentDay,weather.current);
 
+var GenerateUv = function(weather){
+    var uvi = weather.current.uvi;
+    var uvEl = $("#uv");
+    uvEl.text(uvi);
+    console.log(uvi);
+    if (uvi>=11)
+        uvEl.css('background-color','purple');
+    else if(uvi>=8)
+        uvEl.css('background-color','red');
+    else if(uvi>=6)
+        uvEl.css('background-color','orange');
+    else if(uvi>=3)
+        uvEl.css('background-color','yellow');
+    else
+        uvEl.css('background-color','green');
+}
 
 var Search = function(event){
     event.preventDefault();
 
     search = searchHistory[searchHistory.length] = form.children("#search").val()
     currentDay.children()[0].textContent=search;
+
+    
     localStorage.setItem(searchHistory,JSON.stringify(searchHistory));
     var entry = $("<button>").text(search);
     historyEl.prepend(entry);
 
     var geoApi = "http://api.openweathermap.org/geo/1.0/direct?q="+search+"&limit=1&appid=ec93ec889e22ec6a9e1c57a53cc4c613";
     var cordinates = fetch(geoApi).then(function(response){
-        response.json().then(
-
-            GenerateWeatherInfo);
+        response.json().then(GenerateWeatherInfo);
     });
 }
 
-var form = $("form");
-var historyEl = $("#history");
+
 form.on("submit",Search);
 
 //FillWeatherData(currentDay, weather.current);
